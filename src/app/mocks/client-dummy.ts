478fb3acb1b6fea7077d82ecfbc7400ef5b587f1
Andreea Mihalcea <andreea.mihalcea@tremend.com>
2023-02-14 00:20:27 +0200

    tests added in BillService and AppComponent

---
+++ src/app/mocks/client-dummy.ts
@@ -0,0 +1,7 @@
+import { Client } from "../models/client";
+
+export const dummyClientsResponse = [
+  { id: 1, firstName: 'George', lastName: 'Bluth' } as Client,
+  { id: 2, firstName: 'Janet', lastName: 'Weaver' } as Client,
+  { id: 3, firstName: 'Emma', lastName: 'Wong' } as Client,
+];
