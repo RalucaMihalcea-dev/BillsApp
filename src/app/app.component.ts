39c4dd3cf29e16db13d0e5a4efcdf22800af9011
Raluca Mihalcea <Raluca.Mihalcea@endava.com>
2023-02-13 15:26:22 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { BillService } from './services/bill.service';
+import { Client } from './models/client';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'BillsApp';
+  clients: Array<Client> | undefined;
+
+  constructor(private billService: BillService) {}
+
+  ngOnInit(): void {
+    this.loadClients();
+  }
+
+  loadClients(): void {
+    this.billService.getClient().subscribe((data) => {
+      this.clients = data;
+      console.log(data);
+    });
+  }
 }
---
+++ src/app/services/bill.service.ts
@@ -0,0 +1,18 @@
+import { Client } from './../models/client';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class BillService {
+  public API = 'https://localhost:7164/api';
+  public baseUrl = `${this.API}/Client`;
+
+  constructor(private httpClient: HttpClient) {}
+
+  getClient(): Observable<Array<Client>> {
+    return this.httpClient.get<Array<Client>>(this.baseUrl);
+  }
+}
