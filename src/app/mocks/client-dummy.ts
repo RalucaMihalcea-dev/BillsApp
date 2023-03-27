8e2c69c46590d8072d20b7ec79f823c70eb11ea6
Andreea Mihalcea <andreea.mihalcea@tremend.com>
2023-03-27 21:47:03 +0300

    Auto-generated commit [ci skip]

--- src/app/mocks/client-dummy.ts
+++
@@ -1,7 +0,0 @@
-import { Client } from '../models/client';
-
-export const dummyClientsResponse = [
-  { id: 1, firstName: 'George', lastName: 'Bluth' } as Client,
-  { id: 2, firstName: 'Janet', lastName: 'Weaver' } as Client,
-  { id: 3, firstName: 'Emma', lastName: 'Wong' } as Client,
-];
