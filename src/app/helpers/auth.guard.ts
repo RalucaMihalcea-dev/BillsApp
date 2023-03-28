179fa433e01ec92aabf1f634423b66907028fb89
RalucaMihalcea-dev <raluca.mihalcea.e@gmail.com>
2023-03-28 12:02:15 +0300

    fix comments

--- src/app/helpers/auth.guard.ts
+++ src/app/helpers/auth.guard.ts
@@ -1,18 +1,13 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot } from '@angular/router';
-import { AuthService } from '../services/auth.service';
-import { Role } from './../models/role';
+import { Role } from '../models/role';
 import { TokenStorageService } from './../services/token-storage.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthGuard implements CanActivate {
-  constructor(
-    private router: Router,
-    private authService: AuthService,
-    private tokenStorageService: TokenStorageService
-  ) {}
+  constructor(private router: Router, private tokenStorageService: TokenStorageService) {}
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
     const userRoles = this.tokenStorageService.getUserRoles();
@@ -24,22 +19,7 @@ export class AuthGuard implements CanActivate {
     ) {
       return true;
     }
-    // route.data.roles && route.data.roles.
-    // if (userRoles.length != 0) {
-    //   // check if route is restricted by role
-    //   userRoles.forEach((role) => {
-    //     if (route.data.roles && route.data.roles.indexOf(role) === -1) {
-    //       // role not authorised so redirect to home page
-    //       this.router.navigate(['/']);
-    //       return false;
-    //     }
-    //   });
 
-    // authorized so return true
-    // return false;
-    // }
-
-    // not logged in so redirect to login page with the return url
     this.router.navigate(['/login'], { queryParams: { returnUrl: state.url } });
     return false;
   }
--- src/app/helpers/error.interceptor.ts
+++ src/app/helpers/error.interceptor.ts
@@ -2,19 +2,18 @@ import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/c
 import { Injectable } from '@angular/core';
 import { Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
-
-import { AuthService } from '../services/auth.service';
+import { TokenStorageService } from '../services/token-storage.service';
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
-  constructor(private authService: AuthService) {}
+  constructor(private tokenStorageService: TokenStorageService) {}
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
     return next.handle(request).pipe(
       catchError((err) => {
         if ([401, 403].indexOf(err.status) !== -1) {
-          // auto logout if 401 Unauthorized or 403 Forbidden response returned from api
-          // this.authService.logout();
+          this.tokenStorageService.signOut();
+          window.location.reload();
         }
 
         const error = err.error.message || err.statusText;
